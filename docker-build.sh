bc9b20109c73cf0177bccb7a7ce5b815ad8daf6a
George Zhou <alphacodinghub@outlook.com>
2020-08-22 04:28:11 -0700

    update to wp5.5

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build -t alphacodinghub/wordpress-fpm:latest .
-docker build -t alphacodinghub/wordpress-fpm:5.4.2 .
+docker build -t alphacodinghub/wordpress-fpm:5.5 .
 
 docker build -f Dockerfile-for-fpm -t alphacodinghub/php-fpm:7.3-alpine .
 docker build -f Dockerfile-for-fpm -t alphacodinghub/php-fpm:latest .
